2efce5d72da0397aa495f0c662357820fdcbc566
syfrontier <syfrontier@outlook.com>
2016-11-01 20:00:00 +0800

    Add fiel

--- CN_2_UTF-8String.cpp
+++ CN_2_UTF-8String.cpp
@@ -1,4 +1,4 @@
-#include <iconv.h>
+/*#include <iconv.h>
 #include <string.h>
 
 #include <iostream>
@@ -40,6 +40,36 @@ int main(int argc, char *argv[])
 	cout<<in_gb2312<<" "<<out<<endl;
 
 	return 0;
+}
+*/
+
+#include <wchar.h>     
+//#include <locale.h>     
+#include <stdlib.h>     
+#include <stdio.h>     
+#include <string.h>
+
+int main(void)      
+{      
+    //setlocale(LC_ALL,"zh_CN.UTF-8");    
+    wchar_t a[10] = L"ÄãºÃ";
+    char t[64] = {0}, tmp[16] = {0};
+    int i = 0, len;
+    
+    wprintf(L"this is a test !\n"); 
+    len = wcslen(a);
+    wprintf(L"%d\n",len);      
+    //wprintf(L"%X\n",a);
+    
+    for (i = 0;i < len; i++) {
+        bzero(tmp, sizeof(tmp));
+        sprintf(tmp, "\\&#x%X;", a[i]);
+        strcat(t, tmp);
+    }
+    
+    wprintf(L"%s\n", t);
+    
+    return 0;  
 }
 
 
